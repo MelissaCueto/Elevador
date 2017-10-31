17a90dab8d39f96afed80d7b0d130e6566ea4e60
cesar huerta <cesarhuertaescalante@hotmail.com>
2017-10-30 22:00:55 -0600

    Primero Commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Elevador</title>
+    </head>
+    <body>
+        <p></p>
+        
+        <?php
+        // put your code here
+       
+     
+        ?>
+    </body>
+</html>
+ 
